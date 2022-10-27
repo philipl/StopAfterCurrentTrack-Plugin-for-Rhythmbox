f7946aa6e23fbb290e5a55343a286d4ca2aad039
Philip Langdale <philipl@overt.org>
2022-10-27 14:00:59 -0700

    Port to Rhythmbox 3.x

--- StopAfterCurrentTrack/__init__.py
+++ StopAfterCurrentTrack/__init__.py
@@ -1,17 +1,19 @@
 from gi.repository import Gtk, GObject, RB, Peas
 
+from rb3compat import ActionGroup
+from rb3compat import Action
+from rb3compat import ApplicationShell
+from rb3compat import is_rb3
+
+
+
 ui_string = """
 <ui>
     <menubar name="MenuBar">
-        <menu name="ControlMenu" action="Control">
+        <menu name="ToolsMenu" action="Tools">
             <menuitem name="StopAfterCurrentTrack" action="StopAfterCurrentTrack"/>
         </menu>
     </menubar>
-    <toolbar name="ToolBar">
-        <placeholder name="ToolBarPluginPlaceholder">
-            <toolitem name="StopAfterCurrentTrack" action="StopAfterCurrentTrack"/>
-        </placeholder>
-    </toolbar>
 </ui>
 """
 
@@ -22,57 +24,52 @@ class StopAfterCurrentTrackPlugin (GObject.Object, Peas.Activatable):
         super(StopAfterCurrentTrackPlugin, self).__init__()
 
     def do_activate(self):
-        print "Activating Plugin"
+        print("Activating Plugin")
         self.stop_status = False
         shell = self.object
-        self.action = Gtk.ToggleAction(
-                name='StopAfterCurrentTrack',
-                label=('Stop After'),
-                tooltip=('Stop playback after current song'),
-                stock_id=Gtk.STOCK_MEDIA_STOP
-                )
-        self.activate_id = self.action.connect('activate',self.toggle_status,shell)
-        self.action_group = Gtk.ActionGroup(name='StopAfterCurrentTrackPluginActions')
-        self.action_group.add_action(self.action)
+        self.appshell = ApplicationShell(shell)
+
+        self.action_group = ActionGroup(shell, 'StopAfterCurrentTrackPluginActions')
+        self.action = self.action_group.add_action(func=self.toggle_status,
+                                                   action_name='StopAfterCurrentTrack',
+                                                   label=_('Stop After'),
+                                                   action_state=ActionGroup.TOGGLE,
+                                                   action_type='app',
+                                                   tooltip=_('Stop playback after current song'))
+        self.appshell.insert_action_group(self.action_group)
+        self.appshell.add_app_menuitems(ui_string, 'StopAfterCurrentTrackPluginActions', 'tools')
         self.action.set_active(False)
         self.action.set_sensitive(False)
 
-        uim = shell.props.ui_manager
-        uim.insert_action_group(self.action_group,0)
-        self.ui_id = uim.add_ui_from_string(ui_string)
-        uim.ensure_update()
-
         sp = shell.props.shell_player
         self.pec_id = sp.connect('playing-song-changed', self.playing_entry_changed)
-        print "Plugin Activated"
+        print("Plugin Activated")
 
     def do_deactivate(self):
-        print "Deactivating Plugin"
+        print("Deactivating Plugin")
         shell = self.object
-        uim = shell.props.ui_manager
-        uim.remove_ui(self.ui_id)
-        uim.remove_action_group(self.action_group)
+        self.appshell.cleanup()
         sp = shell.props.shell_player
         sp.disconnect (self.pec_id)
         self.action_group = None
         self.action = None
-        print "Plugin Deactivated"
+        print("Plugin Deactivated")
 
-    def toggle_status(self,action,shell):
-        if action.get_active():
+    def toggle_status(self, action, param=None, data=None):
+        if self.action.get_active():
             self.stop_status = True
         else:
             self.stop_status = False
-        print self.stop_status
+        print(self.stop_status)
 
     def playing_entry_changed(self, sp, entry):
-        print "Playing entry changed"
-        print entry
+        print("Playing entry changed")
+        print(entry)
         if entry is not None:
             self.action.set_sensitive(True)
             if self.stop_status:
                 self.action.set_active(False)
-                sp.stop()
+                sp.pause()
         else:
             self.action.set_sensitive(False)
 
